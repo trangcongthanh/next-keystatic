26b3ea842fb06c239893bc759fd03172f489c5ca
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2024-02-05 21:54:03 +0700

    revert

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,6 @@
-import { postBySlug, reader } from "~/keystatic/reader";
+import { postBySlug } from "~/keystatic/reader";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  // const post = await postBySlug(params.slug);
-  const post = await reader.collections.posts.read(params.slug);
-  const content = await post?.content();
-  return <div>{JSON.stringify({ post, content }, null, 2)}</div>;
+  const post = await postBySlug(params.slug);
+  return <div>{JSON.stringify({ post }, null, 2)}</div>;
 }
